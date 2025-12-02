d9edc9848a42e7cb57c3e66a63671c5158058de1
Andrei Simedre <andrei.simedre@gmail.com>
2025-12-02 23:22:42 +0200

    Added xcode project

---
+++ AnimeList/AnimeList/AnimeListApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimeListApp.swift
+//  AnimeList
+//
+//  Created by Andrei Simedre on 02.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct AnimeListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
